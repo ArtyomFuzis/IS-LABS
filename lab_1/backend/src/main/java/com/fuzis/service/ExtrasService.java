9fb7ee3356c4b0d5d1d1ef0de82fea761542972d
Artyom Nazin <artyomnazin@yandex.ru>
2025-11-13 00:22:54 +0300

    Fix backend & installer

---
+++ lab_1/backend/src/main/java/com/fuzis/service/ExtrasService.java
@@ -0,0 +1,4 @@
+package com.fuzis.service;
+
+public class ExtrasService {
+}
---
+++ lab_1/backend/src/main/java/com/fuzis/service/LocationService.java
@@ -0,0 +1,57 @@
+package com.fuzis.service;
+
+import com.fuzis.database.DisciplineRepository;
+import com.fuzis.database.DisciplineRepository;
+import com.fuzis.entity.Discipline;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.FormParam;
+
+import java.util.Collections;
+import java.util.List;
+
+public class DisciplineService {
+    @Inject
+    DisciplineRepository repo;
+
+    @Inject
+    UtilityService utils;
+
+    public List<Discipline> getAll(){
+        return repo.getAll();
+    }
+
+    public List<Discipline> getById(Integer id){
+        return Collections.singletonList(repo.get(id));
+    }
+
+    public void deleteById(Integer id){
+        repo.remove(repo.get(id));
+    }
+
+    public void create(String name,
+                       Integer labs_count,
+                       Integer id){
+        if(id != null) {
+            repo.merge(new Discipline(id,name,labs_count));
+        }
+        else repo.save(new Discipline(name,labs_count));
+    }
+
+    public List<Discipline> getPage(Integer page){
+        return repo.getPage(page);
+    }
+
+    public List<Discipline> getSorted(Integer page, String field, Boolean reversed){
+        if (utils.getFilterableFields(Discipline.class).contains(field)) {
+            return repo.getSortedPage(page,field, reversed);
+        }
+        return null;
+    }
+
+    public List<Discipline> getFiltered(Integer page, String field, String filter){
+        if (utils.getFilterableFields(Discipline.class).contains(field)) {
+            return repo.getFilteredPage(page, field, filter);
+        }
+        return null;
+    }
+}
